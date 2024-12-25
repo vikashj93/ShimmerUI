d31008a4af433f923f99699d9d2b86ae1ca84aeb
vikash <vikash.oswal1993@gmail.com>
2024-12-25 13:57:03 +0530

    transition added to shimmer UI

--- shimmer-ui/src/App.js
+++ shimmer-ui/src/App.js
@@ -5,8 +5,8 @@ import './input.css';
 function App() {
   return (
     <div>
-      <h1 class="text-4xl text-red-600">
-        Hello world!
+      <h1 class="text-4xl underline text-black">
+        Shimmer UI Demo - Meme page
       </h1>
       <Body />
     </div>
--- shimmer-ui/src/components/ShimmerUI.js
+++ shimmer-ui/src/components/ShimmerUI.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import '../index.css';
 
 const ShimmerUI = () => {
   return Array(10)
     .fill(0)
     .map((n,i) => (
-    <div key={i} className='p-5 m-5 border border-black'>
+    <div key={i} className='p-5 m-5 border border-black skeleton'>
         <div className="h-52 w-52 bg-gray-300"></div>
     </div>
   ));
